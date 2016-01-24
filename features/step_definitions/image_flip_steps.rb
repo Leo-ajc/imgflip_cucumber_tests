5a680e0dba8a601ed82ed70829df4e813c0ece9c
Leo-ajc <leo.campbell@gmail.com>
2016-01-24 19:11:08 +0000

    Upload new template step. Adding Long Cat dummy image

--- features/step_definitions/image_flip_steps.rb
+++ features/step_definitions/image_flip_steps.rb
@@ -1,13 +1,19 @@
 Given "I visit imgflip\.com" do
-  visit('https://imgflip.com')
+  visit 'https://imgflip.com'
 end
 
 When "I click the meme creation tool" do
-  pending # Write code here that turns the phrase above into concrete actions
+  find('#nav-make-inner').hover # activate drop down menu.
+  click_link 'Caption a Meme or Image' # click drop down menu element.
 end
 
 When "upload a photo template" do
-  pending # Write code here that turns the phrase above into concrete actions
+  find('#mm-show-upload').click # Click to reveal upload controls.
+  #WTF shitty HTML
+
+  page.attach_file('mm-upload-file', File.absolute_path('./assets/long_cat_is_long.jpg'))
+  fill_in 'customName', with: 'foo'
+  click_button 'Upload'
 end
 
 When "add top and bottom sentences" do
